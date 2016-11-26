a8e215dd9d3a8ce47db67b027cf5dd1b94cea06c
Mirabel Huff <mirabel.huff@gmail.com>
2016-11-26 15:52:28 -0800

    added compPhoto projects 1, 3, 4, 5, 6

--- assets/script.js
+++ assets/script.js
@@ -3,68 +3,87 @@ $(document).ready(function(){
 	//BUTTON SORTS
 	$("#traditional.button").click(function(){
 		$( "#all" ).removeClass( "buttonSelect" );
-        $( "#traditional" ).addClass( "buttonSelect" );
-        $( "#digital" ).removeClass( "buttonSelect" );
-        $( "#threeD" ).removeClass( "buttonSelect" );
+                $( "code" ).removeClass( "buttonSelect" );
+                $( "#traditional" ).addClass( "buttonSelect" );
+                $( "#digital" ).removeClass( "buttonSelect" );
+                $( "#threeD" ).removeClass( "buttonSelect" );
 
-        $(".galleryItem").each(function() {
-        	$(".galleryItem").hide()
-        })
+                $(".galleryItem").each(function() {
+                	$(".galleryItem").hide()
+                })
 
-        $(".traditional").each(function() {
-        	$(".traditional").show().fadeIn(500);
-        })
+                $(".traditional").each(function() {
+                	$(".traditional").show().fadeIn(500);
+                })
 	})
 
 	$("#all.button").click(function(){
 		$( "#all" ).addClass( "buttonSelect" );
-        $( "#traditional" ).removeClass( "buttonSelect" );
-        $( "#digital" ).removeClass( "buttonSelect" );
-        $( "#threeD" ).removeClass( "buttonSelect" );
+                $( "code" ).removeClass( "buttonSelect" );
+                $( "#traditional" ).removeClass( "buttonSelect" );
+                $( "#digital" ).removeClass( "buttonSelect" );
+                $( "#threeD" ).removeClass( "buttonSelect" );
 
-        $(".galleryItem").each(function() {
-        	$(".galleryItem").hide()
-        })
+                $(".galleryItem").each(function() {
+                	$(".galleryItem").hide()
+                })
 
-        $(".galleryItem").each(function() {
-        	$(".galleryItem").show().fadeIn(500);
-        })
+                $(".galleryItem").each(function() {
+                	$(".galleryItem").show().fadeIn(500);
+                })
 	})
 
 	$("#digital.button").click(function(){
 		$( "#all" ).removeClass( "buttonSelect" );
-        $( "#traditional" ).removeClass( "buttonSelect" );
-        $( "#digital" ).addClass( "buttonSelect" );
-        $( "#threeD" ).removeClass( "buttonSelect" );
+                $( "code" ).removeClass( "buttonSelect" );
+                $( "#traditional" ).removeClass( "buttonSelect" );
+                $( "#digital" ).addClass( "buttonSelect" );
+                $( "#threeD" ).removeClass( "buttonSelect" );
 
-        $(".galleryItem").each(function() {
-        	$(".galleryItem").hide();
-        })
+                $(".galleryItem").each(function() {
+                	$(".galleryItem").hide();
+                })
 
-        $(".digital").each(function() {
-        	$(".digital").show().fadeIn(500);
-        })
+                $(".digital").each(function() {
+                	$(".digital").show().fadeIn(500);
+                })
 	})
 
 	$("#threeD.button").click(function(){
 		$( "#all" ).removeClass( "buttonSelect" );
-        $( "#traditional" ).removeClass( "buttonSelect" );
-        $( "#digital" ).removeClass( "buttonSelect" );
-        $( "#threeD" ).addClass( "buttonSelect" );
+                $( "code" ).removeClass( "buttonSelect" );
+                $( "#traditional" ).removeClass( "buttonSelect" );
+                $( "#digital" ).removeClass( "buttonSelect" );
+                $( "#threeD" ).addClass( "buttonSelect" );
 
-        $(".galleryItem").each(function() {
-        	$(".galleryItem").hide();
-        })
+                $(".galleryItem").each(function() {
+                	$(".galleryItem").hide();
+                })
 
-        $(".threeD").each(function() {
-        	$(".threeD").show().fadeIn(500);
-        })
+                $(".threeD").each(function() {
+                	$(".threeD").show().fadeIn(500);
+                })
 	})
 
+        $("#code.button").click(function(){
+                $( "#all" ).removeClass( "buttonSelect" );
+                $( "code" ).addClass( "buttonSelect" );
+                $( "#traditional" ).removeClass( "buttonSelect" );
+                $( "#digital" ).removeClass( "buttonSelect" );
+                $( "#threeD" ).removeClass( "buttonSelect" );
+
+                $(".galleryItem").each(function() {
+                        $(".galleryItem").hide();
+                })
+
+                $(".code").each(function() {
+                        $(".code").show().fadeIn(500);
+                })
+        })
 
 	//OVERLAY
 	//show overlay when clicking on gallery item
-	$(".galleryItem").click(function(){
+	$(".galleryItem").not(".code").click(function(){
 		var num = this.id;
 		var sel = ".content#" + num
 		$("#overlay-container").show();
@@ -72,7 +91,7 @@ $(document).ready(function(){
 	})
 
 	//hide overlay when clicking it
-	$("#overlay-container").click(function(){
+	$("#overlay-container").not(".code").click(function(){
 		$("#overlay-container").hide();
 		$(".content").hide()
 	})
